d72d91658c740a50967410fdc81b18c89c40a0e1
Srujan Mupparapu <srujanmup@gmail.com>
2022-11-14 18:09:42 -0600

    More schema

---
+++ app/src/main/java/com/example/foodhunt/ApiCalls.kt
@@ -0,0 +1,17 @@
+package com.example.foodhunt
+
+// getSellLocations (Places API)
+// api params: current latitude, current longitude, rank by distance, sell location
+// return: list of place results with ids
+
+// getPlaceDetails (Places API)
+// api params: place id
+// return: place details
+
+// getDirections (Directions API)
+// api params: current latitude, current longitude, destination latitude, destination longitude
+// return: list of directions steps
+
+// getSupermarketLocations (Places API)
+// api params: current latitude, current longitude, search radius, supermarket type
+// return: list of place results with ids
---
+++ app/src/main/java/com/example/foodhunt/DataClasses.kt
@@ -0,0 +1,17 @@
+package com.example.foodhunt
+
+// class Recipe {
+//     val id: Int,
+//     val food: Food,
+//     val sellPrice: Int,
+//     val sellLocation: SellLocation,
+//     val ingredients: List<IngredientItem>
+// }
+
+// class IngredientItem {
+//     val id: Int,
+//     val ingredient: Ingredient,
+//     val latitude: Double,
+//     val longitude: Double,
+//     val found: Boolean
+// }
